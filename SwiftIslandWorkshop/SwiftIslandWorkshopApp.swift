bb8216d47bfb873bdbd87f80eca86a5dd5e26f69
Audrey Zebaze <audrey@cheulah.com>
2024-08-27 07:23:21 +0200

    Initial Commit

---
+++ SwiftIslandWorkshop/SwiftIslandWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIslandWorkshopApp.swift
+//  SwiftIslandWorkshop
+//
+//  Created by Audrey SOBGOU ZEBAZE on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIslandWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
